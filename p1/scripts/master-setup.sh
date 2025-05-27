0f696beeb26f6ae29d004abf0743a6c5d3231725
Berat Boşnak <bbosnak@Berats-Mac-mini.local>
2025-05-27 09:54:04 -0700

    Vagrantfile for K3s server and agent on Ubuntu 22.04

--- p1/scripts/master-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-sudo apt update -y && sudo apt upgrade -y
-
-sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110" sh -
-
-cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
