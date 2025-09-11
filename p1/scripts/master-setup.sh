fe4d73e89af65dcbc66c2170c80fb927c6866d31
mustafaUrl <mustafauraler@gmail.com>
2025-09-11 14:45:21 +0300

    wait_next_yukariTarih

--- p1/scripts/master-setup.sh
+++ p1/scripts/master-setup.sh
@@ -3,6 +3,19 @@
 
 sudo apt update -y && sudo apt upgrade -y
 
-sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110" sh -
+sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --write-kubeconfig-mode=644" sh -
 
 cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
+
+
+USER_HOME="/home/vagrant"
+
+
+if [ -d "$USER_HOME" ]; then
+  sudo mkdir -p $USER_HOME/.kube
+  sudo cp /etc/rancher/k3s/k3s.yaml $USER_HOME/.kube/config
+  sudo chown -R vagrant:vagrant $USER_HOME/.kube
+
+  echo "export KUBECONFIG=$USER_HOME/.kube/config" >> $USER_HOME/.bashrc
+  echo "alias k='kubectl'" >> $USER_HOME/.bashrc
+fi
--- p2/scripts/master-setup.sh
+++ p2/scripts/master-setup.sh
@@ -13,3 +13,15 @@ mkdir -p /home/vagrant/.kube
 cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
 
 kubectl create -f /home/vagrant/deployment.yaml -f /home/vagrant/service.yaml -f /home/vagrant/ingress.yaml
+
+USER_HOME="/home/vagrant"
+
+
+if [ -d "$USER_HOME" ]; then
+  sudo mkdir -p $USER_HOME/.kube
+  sudo cp /etc/rancher/k3s/k3s.yaml $USER_HOME/.kube/config
+  sudo chown -R vagrant:vagrant $USER_HOME/.kube
+
+  echo "export KUBECONFIG=$USER_HOME/.kube/config" >> $USER_HOME/.bashrc
+  echo "alias k='kubectl'" >> $USER_HOME/.bashrc
+fi
