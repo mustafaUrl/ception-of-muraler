88fe7457aeaaf7d07874f4d11252687b6f693aa6
mustafaUrl <mustafauraler@gmail.com>
2025-09-19 12:19:38 +0300

    make_up

--- p2/scripts/master-setup.sh
+++ p2/scripts/master-setup.sh
@@ -5,8 +5,6 @@ sudo apt update -y && sudo apt upgrade -y
 
 sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644" sh -
 
-# sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/baremetal/deploy.yaml
-
 cp /vagrant/confs/* /home/vagrant/
 
 mkdir -p /home/vagrant/.kube
